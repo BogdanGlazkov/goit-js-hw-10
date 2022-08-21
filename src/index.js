68ba56049ff744cd617028bbd4808ea26ba0baf3
BogdanGlazkov <bglazkov@i.ua>
2022-08-21 17:11:40 +0300

    Add markup render and styles

--- src/index.js
+++ src/index.js
@@ -14,17 +14,21 @@ refs.input.addEventListener('input', _debounce(searchCountry, DEBOUNCE_DELAY));
 
 function searchCountry(event) {
   clearFields();
+
   if (!event.target.value.trim()) {
     return;
   }
+
   fetchCountries(event.target.value.trim())
     .then(countries => {
-      console.log(countries);
       if (countries.length > 10) {
         Notify.info(
           'Too many matches found. Please enter a more specific name.'
         );
+        return;
       }
+
+      renderMarkup(countries);
     })
     .catch(() => {
       Notify.failure('Oops, there is no country with that name');
@@ -36,4 +40,35 @@ function clearFields() {
   refs.countryInfo.innerHTML = '';
 }
 
-console.log(refs.countryInfo);
+function renderMarkup(countries) {
+  if (countries.length > 1) {
+    const markup = countries
+      .map(({ name: { common }, flags: { svg } }) => {
+        return `<li class="country-list__item">
+          <img class="country-list__img" src="${svg}" alt="${common}">
+          <p class="country-list__name">${common}</p>
+        </li>`;
+      })
+      .join('');
+    refs.countryList.innerHTML = markup;
+  } else {
+    clearFields();
+    const {
+      name: { official },
+      capital,
+      population,
+      flags: { svg },
+      languages,
+    } = countries[0];
+
+    const languagesList = Object.values(languages);
+
+    refs.countryInfo.innerHTML = `
+          <img class="country-info__img" src="${svg}" alt="${official}">
+          <h2 class="country-info__title">${official}</h2>
+          <p class="country-info__field"><b>Capital: </b>${capital}</p>
+          <p class="country-info__field"><b>Population: </b>${population}</p>
+          <p class="country-info__field"><b>Languages: </b>${languagesList}</p>
+        `;
+  }
+}
